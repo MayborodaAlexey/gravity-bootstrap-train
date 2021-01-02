83e343e30ad31316ed27b4579241983bea2990b8
Alexey <Mayboroda>
2021-01-02 21:48:26 +0200

    anime

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 $(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
@@ -18,3 +19,25 @@ $(function () {
     },
   });
 });
+
+// let btn = document.getElementById("cta");
+// let btn2 = document.getElementById("cta2");
+// btn.onclick = function () {
+//   anime({
+//     targets: " .polymorph",
+//     points: [
+//       {
+//         value: "226,117 0,117.5 0,0.5 0,0 39.9,0 72,83",
+//       },
+//       // {
+//       //   value: "70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369",
+//       // },
+//       // {
+//       //   value: "70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369",
+//       // },
+//     ],
+//     easing: "easeOutQuad",
+//     duration: 1200,
+//     loop: false,
+//   });
+// };
